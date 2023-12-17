b8af4c89d4e1cb0b67a941fb1301d508d1d23e02
ploskovams868666 <kit.mariya2018@yandex.ru>
2023-12-17 17:17:13 +0300

    glav 2223

---
+++ BeatBox/app/src/main/java/mai/team4/beatbox/MainActivity.java
@@ -0,0 +1,14 @@
+package mai.team4.beatbox;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
